66f035d73eac97de7a6d71928fa5994486d4ea20
Vanessa <vanessa-almeida@prodesan.com.br>
2022-05-30 12:48:40 -0300

    Estratégia de Menus

--- resources/views/admin/layouts/_partials/header.blade.php
+++ resources/views/admin/layouts/_partials/header.blade.php
@@ -2,8 +2,9 @@
 <header class="w-full items-center bg-white py-2 px-6 hidden sm:flex">
     <div class="w-1/2"></div>
     <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
-        <button @click="isOpen = !isOpen" class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
-            <img src="./user.png">
+        <button @click="isOpen = !isOpen"
+            class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
+            <img src="{{ url('images/user.png') }}">
         </button>
         <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
         <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
@@ -25,19 +26,13 @@
     </div>
 
     <!-- Dropdown Nav -->
-    <nav :class="isOpen ? 'flex': 'hidden'" class="flex flex-col pt-4">
-        <a href="dash.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-            <i class="fas fa-tachometer-alt mr-3"></i>
-            Home
-        </a>
-        <a href="tables.html" class="flex items-center text-white py-4 pl-6 nav-item">
-            <i class="fas fa-align-left mr-3"></i>
-            Listagem
-        </a>
-        <a href="forms.html" class="flex items-center text-white py-4 pl-6 nav-item active-nav-link">
-            <i class="fas fa-table mr-3"></i>
-            Formulários
-        </a>
+    <nav :class="isOpen ? 'flex' : 'hidden'" class="flex flex-col pt-4">
+        @foreach (config('template.menus') as $menu)
+            <a href="{{ $menu['url'] }}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                <i class="{{ $menu['icon'] }} mr-3"></i>
+                {{ $menu['name'] }}
+            </a>
+        @endforeach
         <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
             <i class="fas fa-sign-out-alt mr-3"></i>
             Sair
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\{
+    AdminController
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.home');
+});
 
 Route::get('/', function () {
     return view('welcome');
